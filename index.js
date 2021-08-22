7efbe2423f3dd46652b51e43167966af5039e7b6
maycristina <mayara.crst@gmail.com>
2021-08-22 19:54:53 -0300

    modifiquei-arquivos

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
+
 function toggleMenu() {
     const nav = document.getElementById('nav')
     nav.classList.toggle('active')
 }
-/* 
-function dowloadCV () {
-    const var = "https://drive.google.com/u/0/uc?id=19IvQ1DwcU_zmtP5JdjgO1F3TLbJKtnpR&export=download"
-    document.innerHTML
-=======
-function toggleMenu() {
-    const nav = document.getElementById('nav')
-    nav.classList.toggle('active')
-}
-/* 
-function dowloadCV () {
-    const var = "https://drive.google.com/u/0/uc?id=19IvQ1DwcU_zmtP5JdjgO1F3TLbJKtnpR&export=download"
-    document.innerHTML
->>>>>>> 9399d8ff0ff9693f46c9e3058a6f70c6a734c4f1
-} */
